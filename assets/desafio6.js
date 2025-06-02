190d3ce344481fe7bbd588e938e3670d37710c60
Estudiante <gaiabelends@gmail.com>
2025-06-02 18:45:28 -0300

    agrego ultimo

---
+++ assets/desafio6.js
@@ -0,0 +1,5 @@
+let precioProducto = document.getElementById("precioProducto");
+        precioProducto.innerText = "Precio: $450";
+
+        let nombreProducto = document.querySelector("#nombreProducto");
+        nombreProducto.classList.add("titulo");
